f19dfb1e8c23936bb71a154833c11eba35d40d5f
zhusong <song.zhu@lasong.com.cn>
2021-04-20 14:22:15 +0800

    BasicTypes.kt

--- src/cn/com/lasong/BasicSyntax.kt
+++ src/cn/com/lasong/BasicSyntax.kt
@@ -1,184 +1,190 @@
 package cn.com.lasong
 
-// 1. 函数定义
-// 有返回类型
-fun sum(a: Int, b: Int): Int {
-    return a + b
-}
-fun sum2(a: Int, b: Int) = a + b
-
-// 无返回值
-fun printSum(a: Int, b: Int) {
-    println(a+b)
-}
-fun printSum2(a: Int, b: Int) = println(a+b)
-
-// 可变长参数
-fun vars(vararg v: Int) {
-    for (vt in v)
-        print(vt)
-}
-
-// 2. lambada表达式
-// lambda 表达式的语法格式如下：
-// (parameters)->expression
-// (parameters)->{statements;}
-// 以下是lambda表达式的重要特征:
-//
-// 可选类型声明：不需要声明参数类型，编译器可以统一识别参数值。
-// 可选的参数圆括号：一个参数无需定义圆括号，但多个参数需要定义圆括号。
-// 可选的大括号：如果主体包含了一个语句，就不需要使用大括号。
-// 可选的返回关键字：如果主体只有一个表达式返回值则编译器会自动返回值，大括号需要指定明表达式返回了一个数值。
-fun lambda() {
-    println("lambda")
-    // : lambda类型
-    //  () -> 5
-    val lambda0: () -> Int = { 5 }
-    println(lambda0())
-    // 接收一个参数(数字类型),返回其2倍的值
-    // x -> 2 * x
-    val lambda1: (Int) -> Int = {x -> x * 2}
-    println(lambda1(5))
-    // 接受2个参数(数字),并返回他们的差值
-    // (x, y) -> x – y
-    val lambda2: (Int, Int) -> Int = { x, y -> x-y}
-    println(lambda2(5, 6))
-    val lambda3: (Int, Int) -> Unit = { x, y ->
-        println(x+y);
-        println(x-y);
+class BasicSyntax {
+    // 1. 函数定义
+    // 有返回类型
+    fun sum(a: Int, b: Int): Int {
+        return a + b
     }
-    lambda3(5, 6)
 
-    val sumLambda: (Int, Int) -> Int = { x, y -> x + y }
-    println(sumLambda(1, 2))
-}
+    fun sum2(a: Int, b: Int) = a + b
 
-// 3. 定义常量与变量
-fun vars() {
-    // 可变变量定义：var 关键字
-    // var <标识符> : <类型> = <初始化值>
-    var a: Int = 1
-    println(a)
-    // 变量可修改
-    a += 1
-    println(a)
-    // 不可变变量定义：val 关键字，只能赋值一次的变量(类似Java中final修饰的变量)
-    // val <标识符> : <类型> = <初始化值>
-    val b: Int = 2
-    println(b)
-    // 如果不在声明时初始化则必须提供变量类型
-    val c: Int
-    c = 1
-}
-
-// 4. 字符串模板
-fun string() {
-    // $ 表示一个变量名或者变量值
-    var a = 1
-    // $varName 表示变量值
-    println("a = $a")
-    // ${varName.fun()} 表示变量的方法返回值:
-    println("a + 1 = ${a+1}")
-}
-
-// 5. NULL检查机制
-fun nullCheck() {
-    // 类型后面加?表示可为空
-    var age: String? = "22"
-
-    // 抛出空指针异常
-    // 字段后加!!像Java一样抛出空异常
-    val age_int_throw = age!!.toInt()
-
-    // 不做处理返回 null
-    // 字段后加?可不做处理返回值为 null
-    val age_int_null = age?.toInt()
-
-    // ?配合?:做空判断处理
-    val age_int_default = age?.toInt() ?: -1
-}
-
-// 当一个引用可能为 null 值时, 对应的类型声明必须明确地标记为可为 null。
-//当 str 中的字符串内容不是一个整数时, 返回 null:
-fun parseInt(string: String): Int? {
-    return string.toIntOrNull()
-}
-
-fun nullReturn() {
-    println(parseInt("1"))
-    println(parseInt("1a"))
-    val x = parseInt("1")
-    val y = parseInt("2")
-    // 直接使用 `x * y` 会导致错误, 因为它们可能为 null.
-    if (x != null && y != null) {
-        // 在进行过 null 值检查之后, x 和 y 的类型会被自动转换为非 null 变量
-        println(x * y)
+    // 无返回值
+    fun printSum(a: Int, b: Int) {
+        println(a + b)
     }
-}
 
-// 6. 类型检测及自动类型转换
+    fun printSum2(a: Int, b: Int) = println(a + b)
+
+    // 可变长参数
+    fun vars(vararg v: Int) {
+        for (vt in v)
+            print(vt)
+    }
+
+    // 2. lambada表达式
+    // lambda 表达式的语法格式如下：
+    // (parameters)->expression
+    // (parameters)->{statements;}
+    // 以下是lambda表达式的重要特征:
+    //
+    // 可选类型声明：不需要声明参数类型，编译器可以统一识别参数值。
+    // 可选的参数圆括号：一个参数无需定义圆括号，但多个参数需要定义圆括号。
+    // 可选的大括号：如果主体包含了一个语句，就不需要使用大括号。
+    // 可选的返回关键字：如果主体只有一个表达式返回值则编译器会自动返回值，大括号需要指定明表达式返回了一个数值。
+    fun lambda() {
+        println("lambda")
+        // : lambda类型
+        //  () -> 5
+        val lambda0: () -> Int = { 5 }
+        println(lambda0())
+        // 接收一个参数(数字类型),返回其2倍的值
+        // x -> 2 * x
+        val lambda1: (Int) -> Int = { x -> x * 2 }
+        println(lambda1(5))
+        // 接受2个参数(数字),并返回他们的差值
+        // (x, y) -> x – y
+        val lambda2: (Int, Int) -> Int = { x, y -> x - y }
+        println(lambda2(5, 6))
+        val lambda3: (Int, Int) -> Unit = { x, y ->
+            println(x + y);
+            println(x - y);
+        }
+        lambda3(5, 6)
+
+        val sumLambda: (Int, Int) -> Int = { x, y -> x + y }
+        println(sumLambda(1, 2))
+    }
+
+    // 3. 定义常量与变量
+    fun vars() {
+        // 可变变量定义：var 关键字
+        // var <标识符> : <类型> = <初始化值>
+        var a: Int = 1
+        println(a)
+        // 变量可修改
+        a += 1
+        println(a)
+        // 不可变变量定义：val 关键字，只能赋值一次的变量(类似Java中final修饰的变量)
+        // val <标识符> : <类型> = <初始化值>
+        val b: Int = 2
+        println(b)
+        // 如果不在声明时初始化则必须提供变量类型
+        val c: Int
+        c = 1
+    }
+
+    // 4. 字符串模板
+    fun string() {
+        // $ 表示一个变量名或者变量值
+        var a = 1
+        // $varName 表示变量值
+        println("a = $a")
+        // ${varName.fun()} 表示变量的方法返回值:
+        println("a + 1 = ${a + 1}")
+    }
+
+    // 5. NULL检查机制
+    fun nullCheck() {
+        // 类型后面加?表示可为空
+        var age: String? = "22"
+
+        // 抛出空指针异常
+        // 字段后加!!像Java一样抛出空异常
+        val age_int_throw = age!!.toInt()
+
+        // 不做处理返回 null
+        // 字段后加?可不做处理返回值为 null
+        val age_int_null = age?.toInt()
+
+        // ?配合?:做空判断处理
+        val age_int_default = age?.toInt() ?: -1
+    }
+
+    // 当一个引用可能为 null 值时, 对应的类型声明必须明确地标记为可为 null。
+    //当 str 中的字符串内容不是一个整数时, 返回 null:
+    fun parseInt(string: String): Int? {
+        return string.toIntOrNull()
+    }
+
+    fun nullReturn() {
+        println(parseInt("1"))
+        println(parseInt("1a"))
+        val x = parseInt("1")
+        val y = parseInt("2")
+        // 直接使用 `x * y` 会导致错误, 因为它们可能为 null.
+        if (x != null && y != null) {
+            // 在进行过 null 值检查之后, x 和 y 的类型会被自动转换为非 null 变量
+            println(x * y)
+        }
+    }
+
+    // 6. 类型检测及自动类型转换
 // is 运算符检测一个表达式是否某类型的一个实例
 // 类似于Java中的instanceof关键字
-fun isString(obj: Any): Boolean {
-    if (obj is String) {
-        // 自动转换
-        println("string length is ${obj.length}")
-        return true
+    fun isString(obj: Any): Boolean {
+        if (obj is String) {
+            // 自动转换
+            println("string length is ${obj.length}")
+            return true
+        }
+
+        if (obj is Int && obj !is Long) {
+            println("$obj is Int")
+        }
+        return false
     }
 
-    if (obj is Int && obj !is Long) {
-        println("$obj is Int")
-    }
-    return false
-}
-
-// 7. 区间
+    // 7. 区间
 // 辅以 in 和 !in 形成
-fun range() {
-    // 区间表达式由具有操作符形式 .. 的 rangeTo 函数
-    // 输出1~4
-    println("1~4")
-    for (i in 1..4) print(i)
+    fun range() {
+        // 区间表达式由具有操作符形式 .. 的 rangeTo 函数
+        // 输出1~4
+        println("1~4")
+        for (i in 1..4) print(i)
 
-    // 向下使用downTo
-    // 输出4~1
-    println("\n4~1")
-    for (i in 4 downTo 1) print(i)
+        // 向下使用downTo
+        // 输出4~1
+        println("\n4~1")
+        for (i in 4 downTo 1) print(i)
 
-    // 使用 step 指定步长
-    // 输出13
-    println()
-    for (i in 1..4 step 2) print(i)
+        // 使用 step 指定步长
+        // 输出13
+        println()
+        for (i in 1..4 step 2) print(i)
 
-    // 输出42
-    println()
-    for (i in 4 downTo 1 step 2) print(i)
+        // 输出42
+        println()
+        for (i in 4 downTo 1 step 2) print(i)
 
-    // 使用 until 函数排除结束元素
-    println("\n1~9")
-    for (i in 1 until 10) {   // i in [1, 10) 排除了 10
-        print(i)
+        // 使用 until 函数排除结束元素
+        println("\n1~9")
+        for (i in 1 until 10) {   // i in [1, 10) 排除了 10
+            print(i)
+        }
+
+        // !in 不在范围
+        println("0 is in 1~9?")
+        if (0 !in 1..9)
+            println("0 !in 1~9")
     }
-
-    // !in 不在范围
-    println("0 is in 1~9?")
-    if (0 !in 1..9)
-        println("0 !in 1~9")
 }
+
 
 fun main() {
-    println(sum(1, 2))
-    println(sum2(1, 2))
-    printSum(1, 2)
-    printSum2(1, 2)
-    vars(1, 2)
-    lambda()
-    vars()
-    string()
-    nullCheck()
-    nullReturn()
-    isString(1)
-    isString(2L)
-    isString("1")
-    range()
+    val instance = BasicSyntax()
+    println(instance.sum(1, 2))
+    println(instance.sum2(1, 2))
+    instance.printSum(1, 2)
+    instance.printSum2(1, 2)
+    instance.vars(1, 2)
+    instance.lambda()
+    instance.vars()
+    instance.string()
+    instance.nullCheck()
+    instance.nullReturn()
+    instance.isString(1)
+    instance.isString(2L)
+    instance.isString("1")
+    instance.range()
 }
--- src/cn/com/lasong/BasicTypes.kt
+++ src/cn/com/lasong/BasicTypes.kt
@@ -1,107 +1,214 @@
 package cn.com.lasong
 
-// 1. 字面常量
+class BasicTypes {
+    // 1. 字面常量
 // Byte、Short、Int、Long、Float、Double 数值类型, 就是数字
 // Char在Java中是数值类型, 但在kotlin中是数据类型, 必须要用''表示
-fun typeDefine() {
-    val byte: Byte = 1
-    val short: Short = 1
-    val int: Int = 1
-    val long: Long = 1
-    // 跟Java一样通过L来指定为Long
-    val long2 = 1L
-    // 必须指定f/F来确定为浮点数
-    val float: Float = 1.0f
-    val float2: Float = 1.0F
-    // 不加后缀就是Double类型
-    val double: Double = 1.0
-    val doubleE: Double = 1.0e3 // 1.0*10^3
-    // 下划线使数字常量更易读
-    val int2: Int = 1_000_000
-    // 16进制0x开头
-    val hex: Int = 0xFFFFFF
-    // 2进制0b开头
-    val bit: Int = 0b000000
-}
+    fun typeDefine() {
+        val byte: Byte = 1
+        val short: Short = 1
+        val int: Int = 1
+        val long: Long = 1
+        // 跟Java一样通过L来指定为Long
+        val long2 = 1L
+        // 必须指定f/F来确定为浮点数
+        val float: Float = 1.0f
+        val float2: Float = 1.0F
+        // 不加后缀就是Double类型
+        val double: Double = 1.0
+        val doubleE: Double = 1.0e3 // 1.0*10^3
+        // 下划线使数字常量更易读
+        val int2: Int = 1_000_000
+        // 16进制0x开头
+        val hex: Int = 0xFFFFFF
+        // 2进制0b开头
+        val bit: Int = 0b000000
+    }
 
-// 2. 比较两个数字
+    // 2. 比较两个数字
 // Kotlin 中没有基础数据类型，只有封装的数字类型
 // 每定义的一个变量，其实 Kotlin 帮你封装了一个对象
 // === 完全相等(对象地址)
 // == 值大小相等
-fun compare(){
-    val a: Int = 100
-    val b: Int = 100
-    println("a == b? ${a == b}")
+    fun compare(){
+        val a: Int = 100
+        val b: Int = 100
+        println("a == b? ${a == b}")
 
-    // === 出现警告
-    // Identity equality for arguments of types Int and Int is deprecated
-    // 搜到其中有个示例很疑惑
-    // 从127到128为什么就是不是完全相等了？
-    val ranged = arrayListOf(127, 127)
-    // true
-    println("127 = > ranged[0] === ranged[1]? ${ranged[0] === ranged[1]}")
-    // true
-    println("127 = > ranged[0] == ranged[1]? ${ranged[0] == ranged[1]}")
-    val exclusive = arrayListOf(128, 128)
-    // false
-    println("128 = > exclusive[0] === exclusive[1]? ${exclusive[0] === exclusive[1]}")
-    // true
-    println("128 = > exclusive[0] == exclusive[1]? ${exclusive[0] == exclusive[1]}")
-    // 突然想起来, 数据类型的表示单位是1个字节8位, 范围[-128, 127), 所以是直接同一个地址
-    // "每定义的一个变量，其实 Kotlin 帮你封装了一个对象"
-    // 128超过了1个字节能表示的范围, 所以被封装成了Int, 值虽然相同, 但是是2个对象
-}
+        // === 出现警告
+        // Identity equality for arguments of types Int and Int is deprecated
+        // 搜到其中有个示例很疑惑
+        // 从127到128为什么就是不是完全相等了？
+        val ranged = arrayListOf(127, 127)
+        // true
+        println("127 = > ranged[0] === ranged[1]? ${ranged[0] === ranged[1]}")
+        // true
+        println("127 = > ranged[0] == ranged[1]? ${ranged[0] == ranged[1]}")
+        val exclusive = arrayListOf(128, 128)
+        // false
+        println("128 = > exclusive[0] === exclusive[1]? ${exclusive[0] === exclusive[1]}")
+        // true
+        println("128 = > exclusive[0] == exclusive[1]? ${exclusive[0] == exclusive[1]}")
+        // 突然想起来, 数据类型的表示单位是1个字节8位, 范围[-128, 127), 所以是直接同一个地址
+        // "每定义的一个变量，其实 Kotlin 帮你封装了一个对象"
+        // 128超过了1个字节能表示的范围, 所以被封装成了Int, 值虽然相同, 但是是2个对象
+    }
 
-// 3. 类型转换
+    // 3. 类型转换
 // 在Java中可以进行隐式转换为较大的类型
 // 但Kotlin 不同的类型有不同的表示方式，较小的类型不能隐式转换为较大的类型。
 // 不进行显式转换的情况下我们不能把 Byte 型值赋给一个 Int 变量。
-fun typeCast() {
-    val a: Byte = 1
-    // compile error
+    fun typeCast() {
+        val a: Byte = 1
+        // compile error
 //    val b: Int = a
-    val b: Int = a.toInt()
+        val b: Int = a.toInt()
 
-    // 在运算的时候可以隐式转换, 前提是能根据上下文推断出数据类型
-    val b2: Int = 1 + a // Int + Byte => Int
-}
+        // 在运算的时候可以隐式转换, 前提是能根据上下文推断出数据类型
+        val b2: Int = 1 + a // Int + Byte => Int
+    }
 
-// 4. 位操作符
+    // 4. 位操作符
 // 只用于 Int 与 Long
-fun bitAction(){
-    // 0000 0000 0000 0000 0000 0000 0111 1111
-    val a: Int = 127
-    println("$a shl 1 (a << 1)= ${a shl 1}")
-    println("$a shr 1 (a >> 1)= ${a shr 1}")
-    // 都为1则为1, 否则为0
-    //  0111 1111
-    //& 0000 1111
-    //= 0000 1111
-    //= 15
-    println("$a and 0xFFFF0F (a & 0xFFFF0F)= ${a and 0x0F}")
-    // 有1则为1, 否则为0
-    //  0111 1111
-    //| 0000 1111
-    //= 0111 1111
-    //= 127
-    println("$a or 0x00000F (a | 0x00000F)= ${a or 0x0F}")
-    // 相同则为0, 否则为1
-    //  0111 1111
-    //^ 0000 1111
-    //= 0111 0000
-    //= 112
-    println("$a xor 0x00000F (a ^ 0x00000F)= ${a xor 0x0F}")
-    // 相同则为0, 否则为1
-    //  0111 1111
-    //~
-    //= 1000 0000
-    //= -128
-    println("$a .inv (~a)= ${a.inv()}")
+    fun bitAction(){
+        // 0000 0000 0000 0000 0000 0000 0111 1111
+        val a: Int = 127
+        println("$a shl 1 (a << 1)= ${a shl 1}")
+        println("$a shr 1 (a >> 1)= ${a shr 1}")
+        // 都为1则为1, 否则为0
+        //  0111 1111
+        //& 0000 1111
+        //= 0000 1111
+        //= 15
+        println("$a and 0xFFFF0F (a & 0xFFFF0F)= ${a and 0x0F}")
+        // 有1则为1, 否则为0
+        //  0111 1111
+        //| 0000 1111
+        //= 0111 1111
+        //= 127
+        println("$a or 0x00000F (a | 0x00000F)= ${a or 0x0F}")
+        // 相同则为0, 否则为1
+        //  0111 1111
+        //^ 0000 1111
+        //= 0111 0000
+        //= 112
+        println("$a xor 0x00000F (a ^ 0x00000F)= ${a xor 0x0F}")
+        // 相同则为0, 否则为1
+        //  0111 1111
+        //~
+        //= 1000 0000
+        //= -128
+        println("$a .inv (~a)= ${a.inv()}")
+    }
+
+    // 5. 字符串
+    fun chars() {
+        val char: Char = '0'
+        // kotlin中字符串不能跟数字直接操作
+        // compile error
+//    if (char == 0) {
+//
+//    }
+        println("\'0\' = ${char.toInt()}")
+    }
+
+    // 6. 布尔
+// || – 短路逻辑或
+// && – 短路逻辑与
+// ! - 逻辑非
+    fun boolean() {
+        val a: Int = 1
+        val b: Int = System.currentTimeMillis().toInt()
+
+        if (a > b || b > a) {
+            println("a > b || b > a => a($a) b($b)")
+        }
+
+        if (a > b && b < 0) {
+            println("a > b && b < 0 => a($a) b($b)")
+        }
+
+        if (a != b) {
+            println("a != b => a($a) b($b)")
+        }
+    }
+
+    // 7. 数组
+// 数组用类 Array 实现 一个 size 属性及 get 和 set 方法
+//  [] 重载了 get 和 set 方法
+    fun array() {
+        // 使用函数arrayOf() 泛型类型, 不适用泛型指定类型的情况下可以混合类型
+        println("arrayOf(1, 2, 3)[2] = ${arrayOf(1, 2, 3)[2]}")
+        println("arrayOf<String>(1, 2, 3)[2] = ${arrayOf<String>("s1", "s2", "s3")[2]}")
+        println("arrayOf(1, \"s2\", 3)[2] = ${arrayOf(1, "s2", 3)[2]}")
+        // 指定类型如intArrayOf,longArrayOf，用来表示各个类型的数组，省去了装箱操作，因此效率更高
+        println("intArrayOf(1, 2, 3)[2] = ${intArrayOf(1, 2, 3)[2]}")
+        // 使用工厂方法Array, init自己初始化对象, 默认是it,it可以忽略
+        println("Array(3, init = {i -> i * 2})[2] = ${Array(3, init = {i -> i * 2})[2]}")
+        println("Array(3, init = {it * 2})[2] = ${Array(3, init = {it * 2})[2]}")
+        // 未初始化默认是Unit类型, 就是Kotlin里的所有类型默认值
+        println("Array(3, init = {})[2] = ${Array(3, init = {})[2]}")
+        println("Array(3, init = {0})[2] = ${Array(3, init = {0})[2]}")
+
+        // 数组的遍历
+        val array = arrayOf(1, "s2", 3)
+        for (v in array)
+            println(v)
+
+        for (i in array.indices)
+            println(array[i])
+
+        for ((i,v) in array.withIndex())
+            println("${array[i]} = $v")
+    }
+
+    // 8. 字符串
+    fun string() {
+        val s = "abc"
+        for (c in s)
+            println(c)
+        println(s[1])
+        // Kotlin 支持三个引号 """ 扩起来的字符串，支持多行字符串
+        val mulLines = """ 
+               哈哈哈哈
+            你好
+        """
+        // 原样输出
+        println(mulLines)
+        // 左边空白对齐切割
+        println(mulLines.trimIndent())
+        val mulLines2 = """ 
+               |哈哈哈哈
+            |你好
+        """
+        // 使用指定符号作为边界切割, 默认是|, 可以自定义
+        println(mulLines2.trimMargin())
+        val mulLines3 = """ 
+               #哈哈哈哈
+            #你好
+        """
+        println(mulLines3.trimMargin(marginPrefix = "#"))
+    }
+
+    // 9. 字符串模板
+    fun stringTemplate() {
+        val s = "Hello"
+        println("s is $s and $s.length = ${s.length}")
+        val price = 9.9
+        print("$$price")
+    }
 }
+
+
 fun main() {
-    typeDefine()
-    compare()
-    typeCast()
-    bitAction();
+    val instance = BasicTypes()
+    instance.typeDefine()
+    instance.compare()
+    instance.typeCast()
+    instance.bitAction()
+    instance.chars()
+    instance.boolean()
+    instance.array()
+    instance.string()
+    instance.stringTemplate()
 }
